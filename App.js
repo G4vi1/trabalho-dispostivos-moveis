6120b99b5285851de77fe840e4060f4742e8fb5a
unknown <2412130061@ACADEMICO.IESB.BR>
2026-03-20 11:19:13 -0300

    mytasks

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Titulo } from './util.js';
+import Titulo_padrão from './util.js';
+import { Button, StyleSheet, Text, View} from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{Titulo}</Text>
+      <Text style={{margin: 20}}>{Titulo_padrão}</Text>
+      <Button title="Clique Aqui" />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +23,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    margin: 20,
+  }
 });
